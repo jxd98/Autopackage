2e828d24fd4ff287eb37931fe51b56d4b92e7508
jxd98 <982042339@qq.com>
2018-01-08 18:43:16 +0800

    test2

--- src/AutoPackageTest.java
+++ src/AutoPackageTest.java
@@ -3,7 +3,7 @@ public class AutoPackageTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("自动部署打包");
+		System.out.println("自动部署打包测试11111");
 
 	}
 
